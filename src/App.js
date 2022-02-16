d9de1f7e02e0f59a7b8e752425e63f15045b4d7f
Aite Aigbe <aite@nabla.ooo>
2022-02-16 16:42:54 -0500

    new models in position

--- src/App.js
+++ src/App.js
@@ -3,8 +3,220 @@ import * as THREE from "three";
 import { Canvas } from '@react-three/fiber'
 import { useGLTF, OrbitControls } from '@react-three/drei'
 import { Bloom } from '@react-three/postprocessing'
-import { BlurPass, Resizer, KernelSize } from 'postprocessing'
+import { Resizer, KernelSize } from 'postprocessing'
 import './App.css';
+
+function TicketBooth() {
+  const group = useRef();
+  const { nodes, materials } = useGLTF("/Models/TicketBooth.gltf");
+  return (
+    <group ref={group} dispose={null}>
+      <group position={[0, 110, -270]} rotation={[0, Math.PI / 2, 0]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["Ticket_Booth-Pure_Black_1"].geometry}
+          material={materials["Pure Black_1"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["Ticket_Booth-Metalic_1"].geometry}
+          material={materials.Metalic_1}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["Ticket_Booth-Red_Shadow_2"].geometry}
+          material={materials["Red Shadow_2"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["Ticket_Booth-Wood"].geometry}
+          material={materials.Wood}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["Ticket_Booth-Grey"].geometry}
+          material={materials.Grey}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["Ticket_Booth-Red_shadow_1"].geometry}
+          material={materials["Red shadow_1"]}
+        />
+      </group>
+    </group>
+  );
+}
+
+function Car() {
+  const { nodes, materials } = useGLTF("Models/car.gltf")
+  const group = useRef()
+
+  return (
+    <group ref={group} dispose={null}>
+      <group position={[0, 0, 0]} rotation={[0, Math.PI / 2, 0]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["car1-Dark_Gradient"].geometry}
+          material={nodes["car1-Dark_Gradient"].material}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["car1-Nice_Green"].geometry}
+          material={nodes["car1-Nice_Green"].material}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["car1-Polished_Black"].geometry}
+          material={nodes["car1-Polished_Black"].material}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["car1-Nice_Green_1"].geometry}
+          material={nodes["car1-Nice_Green_1"].material}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["car1-Metalic_2"].geometry}
+          material={materials.Metalic_2}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["car1-Rainbow"].geometry}
+          material={materials.Rainbow}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["car1-Yellow"].geometry}
+          material={materials.Yellow}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["car1-Polished_Black_1"].geometry}
+          material={nodes["car1-Polished_Black_1"].material}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["car1-Pure_White"].geometry}
+          material={materials["Pure White"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["car1-Dark_Gradient_1"].geometry}
+          material={nodes["car1-Dark_Gradient_1"].material}
+        />
+      </group>
+    </group>
+  )
+}
+
+function Cone(xpos) {
+  const group = useRef();
+  const { nodes, materials } = useGLTF("/Models/Cone.gltf");
+  return (
+    <group ref={group} dispose={null}>
+      <group position={[xpos.xpos, -80, 270]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.ConeMat.geometry}
+          material={materials.Mat}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes.ConeMat1.geometry}
+          material={materials.Mat1}
+        />
+      </group>
+    </group>
+  );
+}
+
+function Evans() {
+  const group = useRef();
+  const { nodes, materials } = useGLTF("/Models/Evans.gltf");
+  return (
+    <group ref={group} dispose={null}>
+      <group position={[180, 50, -200]} rotation={[0, Math.PI / 2, 0]}>
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["Evans-Metalic_3"].geometry}
+          material={materials.Metalic_3}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["Evans-Grey_2"].geometry}
+          material={materials.Grey_2}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["Evans-Red_Shadow_3"].geometry}
+          material={materials["Red Shadow_3"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["Evans-Pure_Black_2"].geometry}
+          material={materials["Pure Black_2"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["Evans-Pure_White_3"].geometry}
+          material={materials["Pure White_3"]}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["Evans-Musterd_2"].geometry}
+          material={materials.Musterd_2}
+        />
+        <mesh
+          castShadow
+          receiveShadow
+          geometry={nodes["Evans-Marble_White_2"].geometry}
+          material={materials["Marble White_2"]}
+        />
+      </group>
+    </group>
+  );
+}
+
+function Light() {
+  const light = useMemo(() => new THREE.SpotLight(0xffffff), [])
+  light.castShadow = true;
+
+  light.shadow.mapSize.width = 1024;
+  light.shadow.mapSize.height = 1024;
+  light.distance = 650;
+  light.intensity = 10;
+  light.angle = Math.PI / 3;
+  return (
+    <>
+      <primitive object={light} position={[-280, 270, 300]} />
+      <primitive object={light.target} position={[-150, 100, 0]} />
+    </>
+  )
+}
 
 function Stars() {
   let group = useRef();
@@ -16,12 +228,12 @@ function Stars() {
       emissive: '#ffffff',
       emissiveIntensity: 100,
     });
-    const coords = new Array(200)
+    const coords = new Array(400)
       .fill()
-      .map(i => [
-        Math.random() * 800 - 400,
-        Math.random() * 800 - 400,
-        Math.random() * 800 - 400
+      .map(() => [
+        Math.random() * 2000 - 400,
+        Math.random() * 2000 - 400,
+        Math.random() * 2000 - 400
       ]);
     return [geo, mat, coords];
   }, []);
@@ -35,45 +247,32 @@ function Stars() {
   );
 }
 
-function Car() {
-  const { nodes, materials } = useGLTF("Models/STOP_SIGN.gltf")
-  const group = useRef()
-
+function Floor() {
   return (
-    <group ref={group} dispose={null}
-      scale={.5}>
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.StopSignMat1.geometry}
-        material={nodes.StopSignMat1.material}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.StopSignMat.geometry}
-        material={materials.Mat}
-      />
-      <mesh
-        castShadow
-        receiveShadow
-        geometry={nodes.StopSignMat1_1.geometry}
-        material={nodes.StopSignMat1_1.material}
-      />
-    </group>
-  )
+    <mesh position={[0, -85, 0]} rotation={[Math.PI / -2, 0, 0]}>
+      <planeGeometry args={[2000, 2000, 1, 1]} />
+      <meshStandardMaterial color="black" transparent />
+    </mesh>
+  );
 }
 
 function App() {
   return (
-    <Canvas linear shadowMap camera={{ position: [0, 0, 200], fov: 50 }}>
-      <ambientLight intensity={1} />
-      <pointLight position={[0, 0, 10]} intensity={5} />
-      <pointLight position={[0, 0, -10]} intensity={5} />
-      {/* <spotLight position={[-10, 10, 10]} angle={0.2} intensity={2} castShadow /> */}
+    <Canvas linear shadows shadowMap camera={{ position: [0, 300, 800], fov: 50, far: 3000 }}>
       <Suspense fallback={null}>
+        <ambientLight intensity={0.6} />
+        <spotLight castShadow color={"#ffffff"} position={[200, 150, -200]} intensity={2} />
+        <Evans />
+        <Light />
         <Stars />
         <Car />
+        <TicketBooth />
+        <Cone xpos={-250} />
+        <Cone xpos={-125} />
+        <Cone xpos={0} />
+        <Cone xpos={125} />
+        <Cone xpos={250} />
+        {/* <Floor /> */}
       </Suspense>
       <Bloom
         intensity={100.0} // The bloom intensity.
@@ -85,14 +284,25 @@ function App() {
         luminanceSmoothing={0.025} // smoothness of the luminance threshold. Range is [0, 1]
       />
       <OrbitControls
-        enablePan={true}
+        enablePan={false}
         enableZoom={true}
         enableRotate={true}
         minDistance={100}
-        maxDistance={300} />
+        maxDistance={800}
+        minAzimuthAngle={Math.PI / -3.5}
+        maxAzimuthAngle={Math.PI / 3.5}
+        // minPolarAngle={Math.PI / 3}
+        maxPolarAngle={Math.PI / 2}
+      />
     </Canvas>
   );
 }
 
-useGLTF.preload("/STOP_SIGN.gltf");
+
+useGLTF.preload("/Models/Evans.gltf");
+useGLTF.preload("/Models/Cone.gltf");
+useGLTF.preload("/Models/car.gltf");
+useGLTF.preload("/Models/Cone.gltf");
+useGLTF.preload("/Models/TicketBooth.gltf");
+
 export default App;
